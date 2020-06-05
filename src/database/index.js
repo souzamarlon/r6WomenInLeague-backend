b85ff13e6ab2e3cc8a55b9c44e54f4c63c7071c2
Marlon Souza <42625569+souzamarlon@users.noreply.github.com>
2020-06-05 17:34:53 -0300

    Added option to make connection with string URI

--- src/database/index.js
+++ src/database/index.js
@@ -11,15 +11,20 @@ const models = [User, File, Friendship];
 
 class Database {
   constructor() {
-    // this.connection = new Sequelize(databaseConfig);
-    this.connection = new Sequelize(process.env.DATABASE_URL, {
-      dialect: 'postgres',
-      define: {
-        timestamps: true,
-        underscored: true,
-        underscoredALL: true,
-      },
-    });
+    // Using connection string URI:
+    if (process.env.DATABASE_URL) {
+      this.connection = new Sequelize(process.env.DATABASE_URL, {
+        dialect: 'postgres',
+        define: {
+          timestamps: true,
+          underscored: true,
+          underscoredALL: true,
+        },
+      });
+    } else {
+      // Use this connection if you are not using connection string URI
+      this.connection = new Sequelize(databaseConfig);
+    }
 
     this.init();
     this.associate();
